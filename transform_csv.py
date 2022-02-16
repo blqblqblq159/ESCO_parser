4c49d5c852f05053d706ec8045560bc9f642ec7e
blqblqblq159 <wannes.debreuck@humain.ai>
2022-02-16 17:00:04 +0100

    fixed some preprocessing bug and added job recommender notebook

--- transform_csv.py
+++ transform_csv.py
@@ -6,12 +6,13 @@ expanded_list = []
 for i, row in df.iterrows():
     title = row['job_title']
     uri = row['uri']
-    for eSkill in row['essential_skills'].replace('[','').replace(']','').split(','):
+    for eSkill in row['essential_skills'].replace('[','').replace(']','').replace("'",'').split(','):
         if eSkill:
-            expanded_list.append([title, uri, eSkill, True])
-    for oSkill in row['optional_skills'].replace('[','').replace(']','').split(','):
+            expanded_list.append([title, uri, eSkill.strip(), True])
+    for oSkill in row['optional_skills'].replace('[','').replace(']','').replace("'",'').split(','):
         if oSkill:
-            expanded_list.append([title, uri, oSkill, False])
+            expanded_list.append([title, uri, oSkill.strip(), False])
 expanded_df = pd.DataFrame(expanded_list, columns=['job_title','uri','skill','essential'])
+print(expanded_df.head())
 print(len(expanded_df))
-#expanded_df.to_csv('expanded_title_uri_skills.csv',index=False)
+expanded_df.to_csv('expanded_title_uri_skills.csv',index=False)
