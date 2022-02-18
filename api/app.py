ecf76b5951ea149d403f167987689c7f0c7a8ce6
blqblqblq159 <wannes.debreuck@humain.ai>
2022-02-18 10:45:22 +0100

    added start of API

---
+++ api/app.py
@@ -0,0 +1,59 @@
+import numpy as np
+from flask import Flask
+import pandas as pd
+import json
+app = Flask(__name__)
+
+# Load some stuff
+with open('distance_matrix.npy','rb') as f:
+    distance_mat = np.load(f)
+
+embed_df = pd.read_csv('skill_embed.csv')
+init_sort = pd.read_csv('initial_skill_sort.csv')
+
+#make some functions some functions
+def get_id(skill):
+    for i,skill_name in enumerate(embed_df['name']):
+        if skill_name == skill:
+            return i
+    raise ValueError('skill not found')
+
+def sort_by_total_distance(skill_list):
+    distance_list = []
+    id_list = []
+    for skill in skill_list:
+        id_list.append(get_id(skill))
+    for i in range(len(distance_mat)):
+        if i not in id_list:
+            total_distance = 0
+            for idx in id_list:
+                total_distance += distance_mat[i,idx]
+            distance_list.append(total_distance)
+    distances = np.array(distance_list)
+    idx = np.argsort(distances)
+    top_distances = distances[idx]
+    return top_distances, idx
+
+
+@app.route('/')
+def return_init():
+    names = list(init_sort['name'])
+    weights = list(init_sort['weight'])
+    transposed = {}
+    for a,b in zip(names,weights):
+        transposed[a] = b
+    return json.dumps(transposed)
+
+@app.route('/skills/<skills>')
+def return_closest_skills(skills):
+    skills = [x.strip() for x in skills.split(',')]
+    for skill in skills:
+        print(skill)
+    dists, idx = sort_by_total_distance(skills)
+    skills = list(embed_df['name'][idx])
+    transposed = {}
+    for a,b in zip(skills,dists):
+        transposed[a] = b
+    return json.dumps(transposed)
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
